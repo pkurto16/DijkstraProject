5021e82f5d8a128779359a4d011207be3be74c84
pkurto16 <pkurto23@deerfield.edu>
2022-12-31 20:34:10 -0500

    Made heaps start at index 0 and fixed bugs

--- MyHeap.java
+++ MyHeap.java
@@ -7,7 +7,10 @@ public class MyHeap {
 	
 	public MyHeap() {
 		heapData = new int[maxSize];
-		heapData[0] = 0;
+	}
+	
+	public int size() {
+		return size;
 	}
 	
 	public void insert(int data) {
@@ -15,14 +18,42 @@ public class MyHeap {
 			resize();
 		}
 		//size+1 as index 1 is the first element right now
-		heapData[size+1] = data;
-		insertHelper(data,size+1);
+		heapData[size] = data;
+		insertHelper(data,size);
 		size++;
 	}
+	
+	
+	private void insertHelper(int data, int index) {
+		if(index==0) {
+			return;
+		}
+		int parentIndex = parentOf(index);
+		//'>' = maxHeap and '<' = minHeap
+		if(data>heapData[parentIndex]) {
+			heapData[index] = heapData[parentIndex];
+			heapData[parentIndex] = data;
+			insertHelper(data,parentIndex);
+		}
+		
+	}
 
+	
+	private int leftOf(int index) {
+		return (index)*2+1;
+	}
+	private int rightOf(int index) {
+		return (index)*2+2;
+	}
+	private int parentOf(int index) {
+		return (index-1)/2;
+	}
+
+	
+	
 	private boolean arrayIsFull() {
 		//-1 as size = 1 means that array has size 2
-		if(size>=maxSize-1) {
+		if(size>=maxSize) {
 			return true;
 		}
 		return false;
@@ -32,53 +63,95 @@ public class MyHeap {
 		maxSize*=2;
 		int[] replacement = new int[maxSize];
 		
-		for(int i = 0; i< size+1; i++) {
+		for(int i = 0; i< size; i++) {
 			replacement[i] = heapData[i];
 		}
 		
 		heapData = replacement;
 	}
 	
-	private void insertHelper(int data, int index) {
-		if(index==1) {
+	public int pop() {
+		if(size<=0) {
+			size=0;
+			return 0;
+		}
+		int max = heapData[0];
+		heapData[0] = heapData[size-1];
+		size--;
+		pushDown(heapData[0],0);
+		return max;
+	}
+	
+	private void pushDown(int data, int index) {
+		if(index>=size-1) {
 			return;
 		}
-		
-		//'>' = maxHeap and '<' = minHeap
-		if(data>heapData[index/2]) {
-			heapData[index] = heapData[index/2];
-			heapData[index/2] = data;
-			insertHelper(data,index/2);
+		if(!isLargerThanChildren(index)) {
+			if(heapData[leftOf(index)]>heapData[rightOf(index)]) {
+				heapData[index] = heapData[leftOf(index)];
+				heapData[leftOf(index)] = data;
+				pushDown(data,leftOf(index));
+			}
+			else {
+				heapData[index] = heapData[rightOf(index)];
+				heapData[rightOf(index)] = data;
+				pushDown(data,rightOf(index));
+			}
 		}
 		
 	}
 
+	private boolean isLargerThanChildren(int index) {
+		
+		if(leftOf(index)<size-1&&heapData[index]<heapData[leftOf(index)]) {
+			return false;
+		}
+		if(rightOf(index)<size-1&&heapData[index]<heapData[rightOf(index)]) {
+			return false;
+		}
+		return true;
+	}
+
 	public String visualRepresentation() {
-		return visualRepresentationHelper(1);
+		String heapString = "[";
+		boolean deleteComma = false;
+		
+		for(int i = 0; i<size; i++) {
+			heapString+= heapData[i]+", ";
+			deleteComma=true;
+		}
+		if(deleteComma) {
+			heapString="\n"+heapString.substring(0, heapString.length()-2)+"]\n";
+		}
+		else {
+			heapString="\n"+heapString+"]\n";
+		}
+		
+		return visualRepresentationHelper(0)+heapString;
 	}
 
 	private String visualRepresentationHelper(int index) {
 		String returned = "";
-		if (index > size ) {
+		if (index >= size) {
 			return returned;
 		}
 		if (heapData[index] == 0) {
 			return returned;
 		}
-		returned += visualRepresentationHelper(index*2+1);
+		returned += visualRepresentationHelper(rightOf(index));
 		returned += visualRepresentationLine(index);
-		returned += visualRepresentationHelper(index*2);
+		returned += visualRepresentationHelper(leftOf(index));
 		return returned;
 	}
 
 	private String visualRepresentationLine(int index) {
 		String returned = "";
 		//if at root, just the number is printed
-		if (index/2 != 0) {
+		if (index != 0) {
 			//adds the correct number of spaces and vertical bars in the correct spot to correctly
 			//orient the place where the path  "╚═══════════" or "╔═══════════" starts
 			returned += parentPathSpacesString(index);
-			if (index%2==0) {
+			if (index%2==1) {
 				returned += "╚═══════════";
 			} else {
 				returned += "╔═══════════";
@@ -89,7 +162,7 @@ public class MyHeap {
 
 	private String parentPathSpacesString(int index) {
 		
-		ArrayList<String> parentPathSpaces = parentPathSpaces(index/2, index%2==0,
+		ArrayList<String> parentPathSpaces = parentPathSpaces(parentOf(index), index%2==1,
 				new ArrayList<String>());
 		String returned = "";
 		//since when making the ArrayList we start from the right (at an index) and go left (the place where
@@ -103,17 +176,20 @@ public class MyHeap {
 
 
 	private ArrayList<String> parentPathSpaces(int index, boolean direction, ArrayList<String> stringArray) {
-		if (index/2==0) {
+		if (index==0) {
 			return stringArray;
 		}
-		if (index%2==1 && direction
-				|| index%2==0 && !direction) {
+		if (index%2==0 && direction
+				|| index%2==1 && !direction) {
 			stringArray.add("║           ");
-			stringArray = parentPathSpaces(index/2, !direction, stringArray);
+			stringArray = parentPathSpaces(parentOf(index), !direction, stringArray);
 		} else {
 			stringArray.add("            ");
-			stringArray = parentPathSpaces(index/2, direction, stringArray);
+			stringArray = parentPathSpaces(parentOf(index), direction, stringArray);
 		}
 		return stringArray;
 	}
+	
+	
+	
 }
--- MyHeapTest.java
+++ MyHeapTest.java
@@ -6,9 +6,23 @@ class MyHeapTest {
 	MyHeap testHeap = new MyHeap();
 	@Test
 	void addTest() {
-		for(int i = 0; i<10; i++) {
+		
+		for(int i = 0; i<100; i++) {
 			testHeap.insert((int)(Math.random()*1000));
 		}
+		
+		int prev = testHeap.pop();
+		
+		for(int i = 0; i<100; i++) {
+			
+			if(i%20==0) {
+				System.out.println(testHeap.visualRepresentation());
+			}
+			
+			assertTrue(prev>=testHeap.pop());
+		}
+		assertEquals(0,testHeap.size());
+		
 		System.out.println(testHeap.visualRepresentation());
 	}
 
