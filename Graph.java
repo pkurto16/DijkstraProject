2bd5ba349ddeeda3dbe18d0bd475627a7b86613e
pkurto16 <pkurto23@deerfield.edu>
2023-01-12 21:10:15 -0500

    Working Semi-Tested Graph Class with minimized lines

--- Graph.java
+++ Graph.java
@@ -1,13 +1,10 @@
 import java.util.*;
-
 public class Graph {
 
 	private ArrayList<LinkedList<Integer>> adjacencyList = new ArrayList<LinkedList<Integer>>();
 
 	public Graph(Edge[] edges) {
-		for (Edge e : edges) {
-			addEdge(e);
-		}
+		for (Edge e : edges) addEdge(e);
 	}
 
 	public boolean addEdge(Edge e) {
@@ -15,13 +12,9 @@ public class Graph {
 
 		for (int i = 0; i < 2; i++) {
 			LinkedList<Integer> vertexLL = getVertexLL(edge[i]);
-			
 			if (vertexLL != null) {
-				if (vertexLL.contains(edge[(i + 1) % 2])) {
-					return false;
-				}
+				if (vertexLL.contains(edge[(i + 1) % 2])) return false;
 				vertexLL.add(edge[(i + 1) % 2]);
-				
 			} else {
 				adjacencyList.add(new LinkedList<Integer>(List.of(edge[i], edge[(i + 1) % 2])));
 			}
@@ -31,30 +24,22 @@ public class Graph {
 
 	private LinkedList<Integer> getVertexLL(int vertex) {
 		for (LinkedList<Integer> vertexAdjacency : adjacencyList) {
-			if (vertexAdjacency.getFirst() == vertex) {
-				return vertexAdjacency;
-			}
+			if (vertexAdjacency.getFirst() == vertex) return vertexAdjacency;
 		}
 		return null;
 	}
 
 	public int degree(int vertex) {
-		LinkedList<Integer> vertexLL = getVertexLL(vertex);
-		return vertexLL == null ? 0 : vertexLL.size() - 1;
+		return getVertexLL(vertex) == null ? 0 : getVertexLL(vertex).size() - 1;
 	}
 
 	public int[] getAdjacencyList(int vertex) {
-		LinkedList<Integer> vertexLL = getVertexLL(vertex);
-		int[] returned = new int[vertexLL.size()];
-		vertexLL.forEach(element -> {
-			returned[vertexLL.indexOf(element)] = element;
-		});
+		int[] returned = new int[getVertexLL(vertex).size()];
+		getVertexLL(vertex).forEach(element -> {returned[getVertexLL(vertex).indexOf(element)] = element;});
 		return Arrays.copyOfRange(returned, 1, returned.length);
 	}
 
 	public boolean containsEdge(Edge e) {
-		int[] edge = e.toArray();
-		LinkedList<Integer> startVertexAdjacencies = getVertexLL(edge[0]);
-		return startVertexAdjacencies.contains(edge[1]);
+		return getVertexLL(e.toArray()[0]).contains(e.toArray()[1]);
 	}
 }
